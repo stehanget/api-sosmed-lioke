5b7d4c993802f1b92783993907597ee6e55a7b12
aliefyoga <aliefyogaigusti@gmail.com>
2021-07-27 14:56:27 +0700

    base view modal

--- resources/views/partials/modal/_view.blade.php
+++ resources/views/partials/modal/_view.blade.php
@@ -3,27 +3,68 @@
         <div class="modal-content animate__animated animate__fadeIn">
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-7">
-                        photo
+                    <div class="col-7 feed">
+
+                        <div id="image-feed" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#image-feed" data-bs-slide-to="0" class="active"
+                                    aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#image-feed" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#image-feed" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://dummyimage.com/400x400/1f1a1f/ebebeb.png&text=1" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://dummyimage.com/400x400/1f1a1f/ebebeb.png&text=1" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://dummyimage.com/400x400/1f1a1f/ebebeb.png&text=1" alt="">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#image-feed"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#image-feed"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
                     </div>
-                    <div class="col-5">
-                        <div class="d-flex">
-                            <span class="avatar me-2"><img class="rounded-circle w-100" src="{{ asset('img/team/team-3.jpg') }}" alt=""></span>
+                    <div class="col-5 border-start d-flex flex-column">
+                        <div class="d-flex mb-3">
+                            <span class="avatar me-2">
+                                <img class="rounded-circle w-100" src="{{ asset('img/team/team-3.jpg') }}" alt="">
+                            </span>
                             <span class="user">user pembuat</span>
                         </div>
-                        <div class="">
-                            <span>caption</span>
-                        </div>
                         <div class="comment">
-                            <div class="row">
+                            <p>caption</p>
+                            <div class="row py-3">
                                 <div class="col-auto">
-                                    <div class="avatar-visitor">
-                                        asd
+                                    <div class="avatar">
+                                        <img class="rounded-circle w-100" src="{{ asset('img/team/team-3.jpg') }}"
+                                            alt="">
                                     </div>
                                 </div>
                                 <div class="col">
-                                    <span class="name-visitor me-1">iza</span>
-                                    <span class="">commnt</span>
+                                    <span class="name-visitor nav-link d-inline me-1 p-0">Name</span>
+                                    <span class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+                                        pariatur.</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-comment mt-auto">
+                            <div class="row mb-2">
+                                <div class="col d-flex">
+                                    <textarea class="form-control" name="" rows="1"></textarea>
+                                    <button class="btn btn-get-started m-0 py-2">Kirim</button>
                                 </div>
                             </div>
                         </div>
